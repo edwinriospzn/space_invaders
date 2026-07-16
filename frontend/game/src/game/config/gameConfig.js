2103bb0eab25aa2b081c89cf8d4178001135fda7
edwinriospzn <edwinrios.pzn@gmail.com>
2026-07-16 10:46:59 -0500

    feat(game): create Phaser configuration

---
+++ frontend/game/src/game/config/gameConfig.js
@@ -0,0 +1,14 @@
+import Phaser from 'phaser'
+
+export const gameConfig = {
+  type: Phaser.AUTO,
+
+  width: 800,
+  height: 600,
+
+  backgroundColor: '#1d1d1d',
+
+  parent: 'app',
+
+  scene: []
+}
