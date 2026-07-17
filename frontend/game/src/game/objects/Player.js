0961633fae7825e011bbddb8d3bc886c52508783
edwinriospzn <edwinrios.pzn@gmail.com>
2026-07-17 11:41:11 -0500

    feat(player): render player

--- frontend/game/src/game/objects/Player.js
+++ frontend/game/src/game/objects/Player.js
@@ -1,7 +1,29 @@
 export class Player {
+
     constructor(scene) {
+
         this.scene = scene
 
-        console.log('Player created')
+        this.x = 400
+        this.y = 520
+
+        this.width = 40
+        this.height = 20
+
+        this.create()
     }
+
+    create() {
+
+        this.graphics = this.scene.add.rectangle(
+            this.x,
+            this.y,
+            this.width,
+            this.height,
+            0x00ff00
+        )
+
+        console.log('Player rendered')
+    }
+
 }
