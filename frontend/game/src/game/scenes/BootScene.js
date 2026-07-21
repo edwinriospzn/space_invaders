52ac2b9bf6cb4a5ebc4d923345123b235a30af0e
edwinriospzn <edwinrios.pzn@gmail.com>
2026-07-21 09:21:09 -0500

    feat(enemy): create enemy formation

--- frontend/game/src/game/scenes/BootScene.js
+++ frontend/game/src/game/scenes/BootScene.js
@@ -33,20 +33,34 @@ preload() {
     )
 
     this.player = new Player(this)
-    this.enemy = new Enemy(
-    this,
-    400,
-    120
-    )
+    this.enemies = []
+    const rows = 3
+    const columns = 5
+
+    const startX = 180
+    const startY = 100
+
+    const spacingX = 100
+    const spacingY = 70
+
+    for (let row = 0; row < rows; row++) {
+        for (let column = 0; column < columns; column++) {
+            const x = startX + column * spacingX
+            const y = startY + row * spacingY
+            const enemy = new Enemy(this, x, y)
+            this.enemies.push(enemy)
+        }
+    }
   }
 
   update() {
-    this.frameCount++
-    
-    this.player.update()
-    this.enemy.update()
-    if (this.frameCount % 120 === 0) {
-      console.log(`Frame: ${this.frameCount}`)
-    }
+      this.frameCount++
+      this.player.update()
+      for (const enemy of this.enemies) {
+          enemy.update()
+      }
+      if (this.frameCount % 120 === 0) {
+          console.log(`Frame: ${this.frameCount}`)
+      }
   }
 }
